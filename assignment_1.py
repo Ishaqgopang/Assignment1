37bbdec9d71f0b98775e8e584473e1d1cebdcc35
xoxokhanabdullah <abdullahishaq099@gmail.com>
2024-12-20 18:25:24 +0500

    assignment

---
+++ assignment_1.py
@@ -0,0 +1,7 @@
+name = ("Abdullah Ishaq")
+father_name = ("Balakh Sher Khan")
+current_status =("Student")
+education = ("Agentic AI")
+age = 18
+city = ("Lahore9")
+print(f"my name is {name} ,my father,s name is  {father_name} ,i am a {current_status},studying {education},my age is  {age},i live in {city}")
